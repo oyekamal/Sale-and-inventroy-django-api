7b25e8393926ee9b79013ac56b31b27136dab93f
kamalrapi <muhammad.kamal@rapidev.ae>
2021-04-30 10:34:23 +0500

    commit merge

--- SalesInventory/user/models.py
+++ SalesInventory/user/models.py
@@ -16,7 +16,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=20, null=True, blank=True)
     last_name = models.CharField(max_length=20, null=True, blank=True)
     phone_no = models.PositiveIntegerField(null=True, blank=True)
-    location=models.OneToOneField(Location, on_delete=models.CASCADE)
+    location=models.OneToOneField(Location, on_delete=models.CASCADE, null=True, blank=True, default=None)
     
     def __str__(self):
         return self.email
